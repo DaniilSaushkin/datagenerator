af0593df42e1faee10dded7340af36da2826ef54
daniil saushkin <daniil.saushkin@outlook.com>
2023-12-09 20:34:19 +0700

    change dbset modificator to public

--- datagenerator.core/repository/DatabaseContext.cs
+++ datagenerator.core/repository/DatabaseContext.cs
@@ -5,10 +5,10 @@ namespace datagenerator.core.repository
 {
     public sealed class DatabaseContext : DbContext
     {
-        DbSet<Item> Items { get; set; } = null!;
-        DbSet<User> Users { get; set; } = null!;
-        DbSet<Template> Templates { get; set; } = null!;
-        DbSet<UserData> UserData { get; set; } = null!;
+        public DbSet<Item> Items { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Template> Templates { get; set; } = null!;
+        public DbSet<UserData> UserData { get; set; } = null!;
 
         public DatabaseContext(DbContextOptions<DatabaseContext> options) 
             : base(options) { }
